ad7092743729ae0056061384057cad67681176ea
prabinnnn <prabinbhandari120@gmail.com>
2024-02-11 23:53:17 +0545

    add index

--- utils/sessionManger.js
+++ utils/sessionManger.js
@@ -1,14 +1,19 @@
 const { verifyJWT } = require("./token");
+
 const checkRoles = (sysRoles) => {
   return (req, res, next) => {
     const token = req.headers.token || "";
     if (!token) throw new Error("token is missing");
+
     const data = verifyJWT(token);
     if (!data) throw new Error("permission denied");
+
     const { data: user } = data;
-    const isValidRoles = sysRoles.some((role) => user.roles.includes(role));
-    if (!isValidRoles) throw new Error("permissiong denied");
+    const isValidRole = sysRoles.some((role) => user.roles.includes(role));
+    if (!isValidRole) throw new Error("permission denied");
+
     next();
   };
 };
+
 module.exports = { checkRoles };
