6555623ed28e39e0d29521afe2de9271adc7ec92
prabinnnn <prabinbhandari120@gmail.com>
2024-02-25 17:28:32 +0545

    add index

--- module/users/user.route.js
+++ module/users/user.route.js
@@ -23,7 +23,7 @@ router.post("/login", async (req, res, next) => {
 });
 
 // Example of a role-restricted login route
-router.get("/", checkRoles(["user"]), async (req, res, next) => {
+router.get("/", checkRoles(["admin"]), async (req, res, next) => {
   try {
     const result = await userController.getAll();
     res.json({ msg: result });
@@ -58,6 +58,30 @@ router.post("/reset-password", async (req, res, next) => {
 router.post("/change-password", async (req, res, next) => {
   try {
     const result = await userController.changePassword(req.body);
+    res.json({ msg: result });
+  } catch (e) {
+    next(e);
+  }
+});
+router.get("/", checkRoles(["admin"]), async (req, res, next) => {
+  try {
+    const result = await userController.getById(req.params.id);
+    res.json({ msg: result });
+  } catch (e) {
+    next(e);
+  }
+});
+router.put("/", checkRoles(["admin"]), async (req, res, next) => {
+  try {
+    const result = await userController.updateById(req.params.id, req.body);
+    res.json({ msg: result });
+  } catch (e) {
+    next(e);
+  }
+});
+router.get("/get-profile", checkRoles(["admin"]), async (req, res, next) => {
+  try {
+    const result = await userController.updateById(req.params.id);
     res.json({ msg: result });
   } catch (e) {
     next(e);
--- utils/sessionManger.js
+++ utils/sessionManger.js
@@ -1,14 +1,23 @@
 const { verifyJWT } = require("./token");
+const userModel = require("../module/users/user.model");
 const checkRoles = (sysRoles) => {
-  return (req, res, next) => {
-    const token = req.headers.token || "";
-    if (!token) throw new Error("token is missing");
-    const data = verifyJWT(token);
-    if (!data) throw new Error("permission denied");
-    const { data: user } = data;
-    const isValidRole = sysRoles.some((role) => user.roles.includes(role));
-    if (!isValidRole) throw new Error("permission denied");
-    next();
+  return async (req, res, next) => {
+    try {
+      const token = req.headers.token || "";
+      if (!token) throw new Error("token is missing");
+      const data = verifyJWT(token);
+      if (!data) throw new Error("permission denied");
+      const { data: user } = data;
+      const { email } = user;
+      const userData = await userModel.findOne({ email, isActive: true });
+      if (!userData) throw new Error("User is not found");
+      const isValidRole = sysRoles.some((role) => user.roles.includes(role));
+      if (!isValidRole) throw new Error("permission denied");
+      req.currentUser = userData._id;
+      next();
+    } catch (e) {
+      next(e);
+    }
   };
 };
 
